88f412eaacdf7308e9676dc4ed11ed009898d10f
Jason Kwon <jasonproject628@gmail.com>
2023-10-26 12:14:21 +0900

    feature : 23-10-26
    - main UI 구성 준비 및 백업

--- src/main/frontend/src/pages/Home.jsx
+++ src/main/frontend/src/pages/Home.jsx
@@ -1,9 +1,36 @@
-import HomeLayout from "../components/skeleton/HomeLayout.jsx";
+import Footer from "../components/skeleton/Footer.jsx";
+import {Carousel, Image} from "react-bootstrap";
 
 function Home() {
+
     return (
         <>
-            <HomeLayout/>
+            <Carousel className="container align-self-center col mt-5 mb-5">
+                <Carousel.Item>
+                    <Image src="/mainImages/9bc016bc-cd7a-49cc-a399-47930b00c59f.webp" text="First slide" />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    {/*< text="Second slide" />*/}
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    {/*< text="Third slide" />*/}
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+            <Footer/>
         </>
     );
 }
